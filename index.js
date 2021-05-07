03b78d6d8d58955658ef04b19a123ea0897af867
Maria Santos <ma.ines.santos@gmail.com>
2021-05-07 17:53:18 +0100

    add dependencies

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-// Call modules
+// start modules
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
-// Begin the creation of the server
+// create server
 const app = express();
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || '127.1.0.0';
 
-// Apply modules to the server
 app.use(cors());
 app.use(express.json());
 
-// Routes 127.1.0.0:8080/
+// routes 127.1.0.0:8080/
 app.get('/', function (req, res) {
     res.status(200).json({
         message: 'home'
     });
 });
+
+// call routes 127.1.0.0:8080/user
+app.use('/users', require('./routes/user.routes'));
+
+// call routes 127.1.0.0:8080/tool
+app.use('/tools', require('./routes/gadgets.routes'));
+
+// response to unexpected request
+app.get('*', function(req, res) {
+    res.status(404).json({message: 'not a page sir!'});
+});
+
+// start server
+app.listen(port, host, () => console.log(`App listening at http://${host}:${port}/`));
